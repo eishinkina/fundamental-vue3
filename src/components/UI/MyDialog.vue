e3e1a0cae596d311388617c84ce78e4e4228f9a2
Ekaterina Ishinkina <esiforlearn@gmail.com>
2024-02-27 13:37:22 +0400

    add Mixins. Impurities

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -7,19 +7,10 @@
 </template>
 
 <script>
+import toggleMixin from "./../../mixins/toggleMixin"
 export default {
   name: "MyDialog",
-  props: {
-    show: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  methods: {
-    hideDialog() {
-      this.$emit("update:show", false);
-    },
-  },
+  mixins: [toggleMixin],
 };
 </script>
 
